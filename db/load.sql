b7b7a936ef05c83e7883d29305fead247d59b20c
Haojin Li <hl420@duke.edu>
2022-10-13 15:41:36 -0400

    Restore original database

--- db/load.sql
+++ db/load.sql
@@ -1,48 +1,48 @@
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchase_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
+-- \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.purchase_id_seq',
+--                          (SELECT MAX(id)+1 FROM Purchases),
+--                          false);
 
-\COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.order_id_seq',
-                         (SELECT MAX(id)+1 FROM Orders),
-                         false);                         
+-- \COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.order_id_seq',
+--                          (SELECT MAX(id)+1 FROM Orders),
+--                          false);
 
-\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.uid_seq',
-                         (SELECT MAX(id)+1 FROM Users),
-                         false);
-
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.product_id_seq',
-                         (SELECT MAX(id)+1 FROM Products),
-                         false);
-
-\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.review_id_seq',
-                         (SELECT MAX(id)+1 FROM Reviews),
-                         false);
-
-\COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.seller_id_seq',
-                         (SELECT MAX(id)+1 FROM Sellers),
-                         false);
-
--- DO NOT DELETE THESE COMMENTS
--- THEY ARE ORIGINAL SKELETON CODE THAT BUILDS 
 -- \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
--- -- since id is auto-generated; we need the next command to adjust the counter
--- -- for auto-generation so next INSERT will not clash with ids loaded above:
--- SELECT pg_catalog.setval('public.users_id_seq',
+-- SELECT pg_catalog.setval('public.uid_seq',
 --                          (SELECT MAX(id)+1 FROM Users),
 --                          false);
 
 -- \COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.products_id_seq',
+-- SELECT pg_catalog.setval('public.product_id_seq',
 --                          (SELECT MAX(id)+1 FROM Products),
 --                          false);
 
--- \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.purchases_id_seq',
---                          (SELECT MAX(id)+1 FROM Purchases),
+-- \COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.review_id_seq',
+--                          (SELECT MAX(id)+1 FROM Reviews),
 --                          false);
+
+-- \COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.seller_id_seq',
+--                          (SELECT MAX(id)+1 FROM Sellers),
+--                          false);
+
+-- DO NOT DELETE THESE COMMENTS
+-- THEY ARE ORIGINAL SKELETON CODE THAT BUILDS
+-- \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+-- -- since id is auto-generated; we need the next command to adjust the counter
+-- -- for auto-generation so next INSERT will not clash with ids loaded above:
+SELECT pg_catalog.setval('public.users_id_seq',
+                         (SELECT MAX(id)+1 FROM Users),
+                         false);
+
+\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.products_id_seq',
+                         (SELECT MAX(id)+1 FROM Products),
+                         false);
+
+\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);

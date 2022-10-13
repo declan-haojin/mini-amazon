b7229097f4ae0f1f3d52d34e7bb2e1ef8ad40069
Haojin Li <hl420@duke.edu>
2022-10-13 14:07:23 -0400

    Add updates from the original skeleton and fix some format

--- app/models/product.py
+++ app/models/product.py
@@ -11,19 +11,19 @@ class Product:
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE id = :id
-''',
-                              id=id)
+            SELECT id, name, price, available
+            FROM Products
+            WHERE id = :id
+            ''',
+            id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
     def get_all(available=True):
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE available = :available
-''',
-                              available=available)
+            SELECT id, name, price, available
+            FROM Products
+            WHERE available = :available
+            ''',
+            available=available)
         return [Product(*row) for row in rows]

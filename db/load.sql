121d0492f7023c1d8dced39b26a71a8db462e141
Julie Zhichen Ou <jzo2@duke.edu>
2022-10-15 19:32:50 -0400

    debugged load.sql and changed multiplei csv files

--- db/load.sql
+++ db/load.sql
@@ -1,48 +1,63 @@
--- \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.purchase_id_seq',
---                          (SELECT MAX(id)+1 FROM Purchases),
---                          false);
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.users_uid_seq',
+                         (SELECT MAX(uid)+1 FROM Users),
+                         false);
 
--- \COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.order_id_seq',
---                          (SELECT MAX(id)+1 FROM Orders),
---                          false);
+\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.purchases_purchase_id_seq',
+                         (SELECT MAX(purchase_id)+1 FROM Purchases),
+                         false);
 
--- \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.uid_seq',
---                          (SELECT MAX(id)+1 FROM Users),
---                          false);
+\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.products_product_id_seq',
+                         (SELECT MAX(product_id)+1 FROM Products),
+                         false);
 
--- \COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.product_id_seq',
---                          (SELECT MAX(id)+1 FROM Products),
---                          false);
+\COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.orders_order_id_seq',
+                         (SELECT MAX(order_id)+1 FROM Orders),
+                         false);
 
--- \COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.review_id_seq',
---                          (SELECT MAX(id)+1 FROM Reviews),
---                          false);
+\COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.sellers_seller_id_seq',
+                         (SELECT MAX(seller_id)+1 FROM Sellers),
+                         false);
 
--- \COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
--- SELECT pg_catalog.setval('public.seller_id_seq',
---                          (SELECT MAX(id)+1 FROM Sellers),
---                          false);
+\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.reviews_review_id_seq',
+                         (SELECT MAX(review_id)+1 FROM Reviews),
+                         false);
+
+\COPY Cart FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY Inventories FROM 'Inventories.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY UsersPurchases FROM 'users_purchases.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY PurchasesOrders FROM 'purchases_orders.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY OrdersProducts FROM 'orders_products.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY OrdersSellers FROM 'orders_sellers.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY UsersOrders FROM 'users_orders.csv' WITH DELIMITER ',' NULL '' CSV
+
 
 -- DO NOT DELETE THESE COMMENTS
 -- THEY ARE ORIGINAL SKELETON CODE THAT BUILDS
 -- since id is auto-generated; we need the next command to adjust the counter
 -- for auto-generation so next INSERT will not clash with ids loaded above:
-\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.users_id_seq',
-                         (SELECT MAX(id)+1 FROM Users),
-                         false);
+-- \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.users_id_seq',
+--                          (SELECT MAX(id)+1 FROM Users),
+--                          false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.products_id_seq',
-                         (SELECT MAX(id)+1 FROM Products),
-                         false);
+-- \COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.products_id_seq',
+--                          (SELECT MAX(id)+1 FROM Products),
+--                          false);
 
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
+-- \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.purchases_id_seq',
+--                          (SELECT MAX(id)+1 FROM Purchases),
+--                          false);

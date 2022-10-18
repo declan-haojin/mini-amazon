565c85974ab349fe176c7dff039bf594686bd1b4
Tingnan Hu <th311@duke.edu>
2022-10-17 20:48:25 -0400

    changed review files

--- app/review.py
+++ app/review.py
@@ -5,11 +5,18 @@ from .models.review import Review
 from flask import Blueprint
 bp = Blueprint('reviews', __name__)
 
-
-
-@bp.route('/review/hw4', methods=['GET'])
+@bp.route('/review/search', methods=['GET'])
 def search():
-    k = 5
-    reviews = Review.get_k_most_expensive(k)
-    return render_template('hw4_social.html', reviews = reviews)
+    reviews = []
+    return render_template('reviews/search.html', reviews=reviews)
 
+
+@bp.route('/reviews/hw4', methods=['GET'])
+def search_hw4():
+    k = request.args.get('k')
+    uid = request.args.get('uid')
+    if k is None:
+        reviews = []
+    else:
+        reviews = Review.get_k_most_recent(k, uid)
+    return render_template('hw4_review.html', reviews=reviews)

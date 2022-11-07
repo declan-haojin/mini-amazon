937255d502198615b48d3607d7e2001b5d556b7d
Haojin Li <hl420@duke.edu>
2022-11-07 11:48:12 -0500

    Comment review.py

--- app/models/product.py
+++ app/models/product.py
@@ -4,7 +4,6 @@ from flask import current_app as app
 class Product:
     def __init__(self, product_id, name, price, available):
         self.product_id = product_id
-        self.
         self.name = name
         self.price = price
         self.available = available

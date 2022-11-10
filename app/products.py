acdfb9ef34bd01b187f671e3fdef1f4138da5293
Haojin Li <hl420@duke.edu>
2022-11-10 00:07:03 -0500

    Change order of functions

--- app/products.py
+++ app/products.py
@@ -17,16 +17,6 @@ def search():
     return render_template('products/search.html', products = products)
 
 
-@bp.route('/product/hw4', methods=['GET'])
-def search_hw4():
-    k = request.args.get('k')
-    if k is None:
-        products = []
-    else:
-        products = Product.get_k_most_expensive(k)
-    return render_template('hw4_product.html', products = products)
-
-
 @bp.route('/product/<product_id>', methods = ['GET'])
 def index(product_id):
     if product_id is None:
@@ -35,3 +25,13 @@ def index(product_id):
         product = Product.get(product_id)
         # print(product)
     return render_template('products/index.html', product = product)
+
+
+@bp.route('/product/hw4', methods=['GET'])
+def search_hw4():
+    k = request.args.get('k')
+    if k is None:
+        products = []
+    else:
+        products = Product.get_k_most_expensive(k)
+    return render_template('hw4_product.html', products = products)

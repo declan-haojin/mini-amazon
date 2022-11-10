4b259d7a188d167049756a19c371ed9b138cc10d
Zirui Yin <zy132@duke.edu>
2022-11-09 23:01:25 -0500

    fixed front-end bug

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -20,6 +20,19 @@ class Inventory:
         return rows
 
     @staticmethod
+    def get_by_sid(sid):
+        rows = app.db.execute('''
+        SELECT Products.product_id, Products.name, Inventories.inventory_quantity
+        FROM Inventories, Products
+        WHERE Inventories.seller_id = :sid
+        AND Inventories.product_id = Products.product_id
+        ORDER BY Products.product_id
+        ''',
+        sid=sid)
+        print(rows)
+        return rows
+
+    @staticmethod
     def add_item_to_inventory(sid, pid, qty):
         row = app.db.execute('''
         SELECT Inventories.seller_id, Inventories.product_id, Inventories.inventory_quantity
@@ -30,12 +43,17 @@ class Inventory:
         sid = sid,
         pid = pid
         )
+
         if row == []:
             qty = int(qty)
             app.db.execute('''
-            
-            ''')
-            return False
+            INSERT INTO Inventories VALUES (:sid, :pid, :qty)
+            ''',
+            sid = sid,
+            pid = pid,
+            qty = qty
+            )
+            return ["Added new item", sid, pid, qty]
         else:
             qty = int(qty)
             qty += row[0][2]
@@ -48,4 +66,4 @@ class Inventory:
             pid = pid,
             qty = qty
             )
-            return True
+            return ["Modified count", sid, pid, qty]
--- app/sellers.py
+++ app/sellers.py
@@ -13,8 +13,6 @@ def sellers_hw4():
         products = []
     else:
         products = Inventory.get_by_sid(sid)
-
-        
     return render_template('hw4_seller.html', products = products)
 
 @bp.route('/seller/add', methods=['GET'])
@@ -22,5 +20,8 @@ def sellers_add():
     sid = request.args.get('sid')
     pid = request.args.get('pid')
     qty = request.args.get('qty')
-    arg = Inventory.add_item_to_inventory(sid, pid, qty)
+    if request.args == {}:
+        arg = []
+    else:
+        arg = Inventory.add_item_to_inventory(sid, pid, qty)
     return render_template('seller_add.html', arg = arg)

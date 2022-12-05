d245ee5b23eacb1cdd4c62662022744cff6d54c0
Julie Zhichen Ou <jzo2@duke.edu>
2022-12-04 21:56:05 -0500

    in progress

--- app/carts.py
+++ app/carts.py
@@ -11,7 +11,7 @@ bp = Blueprint('carts', __name__)
 def order():
     return render_template('carts/order.html')
 
-@bp.route('/cart/detail', methods=['GET'])
+@bp.route('/cart/detail', methods=['GET', 'POST'])
 def detail():
     carts = Cart.get_all(uid=session['user'], quantity=0)
     return render_template('carts/detail.html', carts = carts)
@@ -25,3 +25,13 @@ def search():
     else:
         carts = Cart.get_all_by_uid(uid)
     return render_template('hw4_cart.html', carts = carts)
+
+
+@bp.route('/cart/delete', methods=['GET', 'POST'])
+def delete():
+    if request.form.get('delete'):
+        Cart.remove_item(request.form.get('delete'))
+        return redirect('/cart/detail')
+    else:
+        carts = Cart.get_all(uid=session['user'], quantity=0)
+        return render_template('carts/detail.html', carts = carts)
--- app/models/cart.py
+++ app/models/cart.py
@@ -16,45 +16,60 @@ class Cart:
 
     @staticmethod
     def get(id):
-        rows = app.db.execute('''
-SELECT id, uid, pid, time_added_to_cart
-FROM Cart
-WHERE id = :id
-''',
+        rows = app.db.execute(
+        '''
+        SELECT id, uid, pid, time_added_to_cart
+        FROM Cart
+        WHERE id = :id
+        ''',
                               id=id)
         return Cart(*(rows[0])) if rows else None
 
     @staticmethod
     def get_all_by_uid_since(uid, since):
-        rows = app.db.execute('''
-SELECT id, uid, pid, time_added_to_cart
-FROM Cart
-WHERE uid = :uid
-AND time_added_to_cart >= :since
-ORDER BY time_added_to_cart DESC
-''',
+        rows = app.db.execute(
+        '''
+        SELECT id, uid, pid, time_added_to_cart
+        FROM Cart
+        WHERE uid = :uid
+        AND time_added_to_cart >= :since
+        ORDER BY time_added_to_cart DESC
+        ''',
                               uid=uid,
                               since=since)
         return [Cart(*row) for row in rows]
 
     @staticmethod
     def get_all_by_uid(uid):
-        rows = app.db.execute('''
-SELECT c.uid, p.name, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
-FROM Products p, Cart c
-WHERE c.uid = :uid AND p.product_id = c.product_id
-''',
+        rows = app.db.execute(
+        '''
+        SELECT c.uid, p.name, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
+        FROM Products p, Cart c
+        WHERE c.uid = :uid AND p.product_id = c.product_id
+        ''',
                               uid=uid)
         return [Cart(*row) for row in rows]
 
 
     @staticmethod
     def get_all(uid, quantity):
-        rows = app.db.execute('''
-SELECT c.uid, p.name, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
-FROM Products p, Cart c
-WHERE c.uid = :uid AND p.product_id = c.product_id
-''',
+        rows = app.db.execute(
+        '''
+        SELECT c.uid, p.name, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
+        FROM Products p, Cart c
+        WHERE c.uid = :uid AND p.product_id = c.product_id
+        ''',
                               uid=uid,
                               cart_quantity=quantity)
         return [Cart(*row) for row in rows]
+
+#Delete item
+    @staticmethod 
+    def remove_item(product_id): 
+        app.db.execute(
+        '''
+        DELETE FROM Cart
+        WHERE product_id = :product_id
+        ''', 
+        product_id = product_id)
+        return

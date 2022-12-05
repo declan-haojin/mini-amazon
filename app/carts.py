0946d0b98337784884d17e61a554fc84cedf7e6a
Julie Zhichen Ou <jzo2@duke.edu>
2022-12-04 22:38:18 -0500

    delete working

--- app/carts.py
+++ app/carts.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask import request
+from flask import request, redirect
 from .models.cart import Cart
 from .models.product import Product
 from flask import session
@@ -13,8 +13,12 @@ def order():
 
 @bp.route('/cart/detail', methods=['GET', 'POST'])
 def detail():
-    carts = Cart.get_all(uid=session['user'], quantity=0)
-    return render_template('carts/detail.html', carts = carts)
+    if request.method == "POST":
+        Cart.remove_item(request.form['delete'])
+        return redirect('/cart/detail')
+    else:
+        carts = Cart.get_all(uid=session['user'], quantity=0)
+        return render_template('carts/detail.html', carts = carts)
 
 
 @bp.route('/cart/hw4', methods=['GET'])
@@ -25,13 +29,3 @@ def search():
     else:
         carts = Cart.get_all_by_uid(uid)
     return render_template('hw4_cart.html', carts = carts)
-
-
-@bp.route('/cart/delete', methods=['GET', 'POST'])
-def delete():
-    if request.form.get('delete'):
-        Cart.remove_item(request.form.get('delete'))
-        return redirect('/cart/detail')
-    else:
-        carts = Cart.get_all(uid=session['user'], quantity=0)
-        return render_template('carts/detail.html', carts = carts)
--- app/models/cart.py
+++ app/models/cart.py
@@ -6,12 +6,13 @@ class Cart:
     This is just a TEMPLATE for Cart, you should change this by adding or
         replacing new columns, etc. for your design.
     """
-    def __init__(self, uid, product_id, cart_quantity, unit_price, total_price):
+    def __init__(self, uid, name, product_id, cart_quantity, unit_price, total_price):
         self.uid = uid
+        self.name = name
         self.product_id = product_id
         self.cart_quantity = cart_quantity
         self.unit_price = unit_price
-        self.total_price = total_price
+        self.total_price = cart_quantity*unit_price
 
 
     @staticmethod
@@ -55,7 +56,7 @@ class Cart:
     def get_all(uid, quantity):
         rows = app.db.execute(
         '''
-        SELECT c.uid, p.name, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
+        SELECT c.uid, p.name, p.product_id, cart_quantity, unit_price, (unit_price * cart_quantity) AS total_price
         FROM Products p, Cart c
         WHERE c.uid = :uid AND p.product_id = c.product_id
         ''',
